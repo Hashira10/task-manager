04f4ed96b7ea674fb98d52eaafce971bfed5bf31
Olga <ten04062004@gmail.com>
2025-05-16 21:22:05 +0500

    add Dockerfile and docker-compose setup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,35 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        laravel({
-            input: ['resources/js/app.js'],
-            refresh: true,
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          crypto: true,
         }),
-    ],
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          crypto: true,
+        }),
+      ],
+    },
+  },
 });
